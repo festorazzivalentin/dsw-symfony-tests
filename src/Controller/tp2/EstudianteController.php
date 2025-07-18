e539e83e696eac8e7da24471653e55659573b22a
Valentín Festorazzi <festorazzivalentin9@gmail.com>
2025-07-18 10:42:45 -0300

    Trabajo práctico 2 - Twig

--- src/Controller/tp2/EstudianteController.php
+++ src/Controller/tp2/EstudianteController.php
@@ -21,4 +21,40 @@ final class EstudianteController extends AbstractController
             'estudiantes' => $estudiantes_json
         ]);
     }
+
+    #[Route('/estudiantes/cursos', name: 'ver_cursos_estudiantes')]
+    public function estudiantesCursosAction(): Response {
+        $cursos = [
+            'A1' => [
+                ['id' => 1, 'nombre' => "María", 'nota' => 8.5],
+                ['id' => 2, 'nombre' => "Alejandro", 'nota' => 8.2]
+            ],
+            'A2' => [
+                ['id' => 3, 'nombre' => "Valentín", 'nota' => 9],
+                ['id' => 4, 'nombre' => "Adriano", 'nota' => 10]
+            ]
+        ];
+        
+        return $this->render('tp2/cursos_por_estudiantes.html.twig', [
+            'cursos' => $cursos
+        ]);
+        
+    }
+
+    #[Route('/estudiantes/vista', name: 'vista_estudiantes')]
+    public function vistaEstudiantesAction(): Response {
+
+        $estudiantes = [
+            ['nombre' => 'Juan', 'nota' => 8.5],
+            ['nombre' => 'María', 'nota' => 7.0],
+            ['nombre' => 'Pedro', 'nota' => 9.0]
+        ];
+
+        return $this->render('tp2/vista.html.twig', [
+            'estudiantes' => $estudiantes
+        ]);
+
+    }
+
+    
 }
